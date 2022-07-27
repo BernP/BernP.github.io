8099a61defe50b3b96bc5e77996be2f945d900b2
BernP <103436789+BernP@users.noreply.github.com>
2022-07-27 14:04:30 -0300

    Update submenu_tools.js

--- submenu_tools.js
+++ submenu_tools.js
@@ -1,5 +1,8 @@
 import { Decrypt, Encrypt } from './encrypt.js';
 
+import { 
+    HashComplex 
+} from './aux_functions.js';
 
 
 function ShowSalt() {
@@ -99,12 +102,90 @@ function TurnOutputOn() {
 }
 
 function BetweenEncAndDec() {
-    TurnOutputOn();
-    if (document.getElementById("sub-menu-encrypter").classList.contains("crypter-swap-button-on") == true) Encrypt();
-    else Decrypt();
-    return false;
+    if(ConfirmTime() == false) return;
+    else{
+        SetLoadTime();
+        TurnOutputOn();
+        if (document.getElementById("sub-menu-encrypter").classList.contains("crypter-swap-button-on") == true) Encrypt();
+        else Decrypt();
+        return false;
+    }
+    
 }
 
+function TimeToEnd(){
+    let numb = document.getElementById('complexity').value;
+
+    let test1 = performance.now();
+    for(let i = 0; i < 5; i++)
+        {
+            HashComplex("timeCounter", 3);
+        }
+        
+        let avrgS;
+        let avrgE;
+        const quantity = 3;
+
+        
+
+        let high = 0;
+        for(let i = 0; i<quantity; i++)
+        {
+            avrgS =  performance.now();
+            HashComplex("timeCounter", 3);
+            avrgE  =  performance.now();
+            if(avrgE - avrgS > high) high = avrgE - avrgS;
+        }
+
+        let probTimeOut = 5*(10**(numb-3))*high;
+        probTimeOut /= 1000;
+        
+        return (probTimeOut);
+    
+    
+}
+function Checker(time, unit){
+    var result = confirm("This will take nearly " + Math.round(time) + unit + " to be done.\nAre you sure to proceed?");
+    return result;
+
+}
+function ConfirmTime(){
+    var time = TimeToEnd();
+    let secInMin = 60;
+    let secInHour = 3600;
+    let secInDay = 86400;
+
+    if(time >= 1){
+        if(time < secInMin)
+        {
+            return Checker(time, " second(s)");
+        }
+        else if(time>= secInMin && time < secInHour)
+        {
+            time /= secInMin;
+            return Checker(time, " minute(s)");
+        }
+        else if(time>= secInHour && time < secInDay){
+            time /= secInHour;
+            return Checker(time, " hour(s)");
+
+        }
+        else{
+            time /= secInDay;
+            return Checker(time, " day(s)");
+        }
+    }
+}
+function SetLoadTime(){
+  
+    let progressBar = document.querySelector('.load-bar');
+    progressBar.setAttribute('id', 'play-animation');
+    let animationLoad = document.getElementById("play-animation");
+    
+    let stringTime =  String('progress-anim '+ TimeToEnd()+'s' + ' forwards');
+    animationLoad.style.animation = stringTime;
+    
+}
 
 export {
     ShowSalt,
